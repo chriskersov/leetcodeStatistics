28b138f86f63a3d917c5cc298ebbe0d2aa99a8bf
chriskersov <chris@kersov.com>
2024-12-28 19:51:55 +0000

    initialisation

---
+++ LeetCode Statistics/LeetCode Statistics/LeetCode_StatisticsApp.swift
@@ -0,0 +1,20 @@
+//
+//  LeetCode_StatisticsApp.swift
+//  LeetCode Statistics
+//
+//  Created by chris kersov on 28/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct LeetCode_StatisticsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
