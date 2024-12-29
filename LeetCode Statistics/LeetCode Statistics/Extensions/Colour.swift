d4d97490b1d17fe209de1b6940142418f2faf79f
chriskersov <chris@kersov.com>
2024-12-29 23:58:19 +0000

    figuring out the fundamentals

---
+++ LeetCode Statistics/LeetCode Statistics/Extensions/Colour.swift
@@ -0,0 +1,14 @@
+//
+//  Colour.swift
+//  LeetCode Statistics
+//
+//  Created by chris kersov on 29/12/2024.
+//
+
+import SwiftUI
+
+extension Color {
+    static let backgroundColorDark: Color = Color(red: 25/255, green: 25/255, blue: 25/255)
+    
+    
+}
