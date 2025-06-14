ea14510de099587b4e25a40e6cbce36936564d2d
chris <chris@kersov.com>
2025-06-14 21:37:38 +0100

    home page fixes and light mode copmleted

---
+++ LeetCode Statistics/LeetCode Statistics/Extensions/ColourExtensions.swift
@@ -0,0 +1,8 @@
+//
+//  ColourExtensions.swift
+//  LeetCode Statistics
+//
+//  Created by chris kersov on 10/04/2025.
+//
+
+import Foundation
---
+++ LeetCode Statistics/LeetCode Statistics/ThemeManager.swift
@@ -0,0 +1,8 @@
+//
+//  ThemeManager.swift
+//  LeetCode Statistics
+//
+//  Created by chris kersov on 10/04/2025.
+//
+
+import Foundation
